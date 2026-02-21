278e08a10f1475e33b8300955370cb5578852785
emon-101 <emon26.ugv@gmail.com>
2026-02-22 02:37:38 +0600

    write some function for interview & rejection button

--- scripts/utility.js
+++ scripts/utility.js
@@ -11,3 +11,54 @@ function showOne(id) {
     const selected = document.getElementById(id);
     selected.classList.remove('hidden');
 }
+
+// card section
+function interviewClick(id, card) {
+    const badge = document.getElementById(id);
+    const addCard = document.getElementById(card);
+    badge.innerText = 'INTERVIEW';
+    badge.classList.add('bg-green-600');
+    badge.classList.remove('bg-red-600');
+    badge.classList.add('text-white');
+
+    const interviewCount = document.getElementById('interview-count');
+    const interviewCountValue = interviewCount.innerText;
+    let newCount = Number(interviewCountValue) + 1;
+    interviewCount.innerText = newCount;
+
+    const interviewSection = document.getElementById('interview-section');
+    const defaultInterview = document.getElementById('default-interview');
+    if(newCount > 0) {
+        defaultInterview.classList.add('hidden');
+    } else {
+        defaultInterview.classList.remove('hidden');
+    }
+    
+    const clonedCard = addCard.cloneNode(true);
+    interviewSection.appendChild(clonedCard);
+    
+}
+function rejectedClick(id, card) {
+    const badge = document.getElementById(id);
+    const addCard = document.getElementById(card);
+    badge.innerText = 'REJECTED';
+    badge.classList.add('bg-red-600');
+    badge.classList.remove('bg-green-600');
+    badge.classList.add('text-white');
+
+    const rejectedCount = document.getElementById('rejected-count');
+    const rejectedCountValue = rejectedCount.innerText;
+    let newCount = Number(rejectedCountValue) + 1;
+    rejectedCount.innerText = newCount;
+
+    const rejectionSection = document.getElementById('rejection-section');
+    const defaultRejection = document.getElementById('default-rejection');
+    if(newCount > 0) {
+        defaultRejection.classList.add('hidden');
+    } else {
+        defaultRejection.classList.remove('hidden');
+    }
+    
+    const clonedCard = addCard.cloneNode(true);
+    rejectionSection.appendChild(clonedCard);
+}
