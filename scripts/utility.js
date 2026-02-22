e6f57ac4d8083eb90772ceededef9cc7f9123837
emon-101 <emon26.ugv@gmail.com>
2026-02-22 19:51:29 +0600

    update some functionalities for interview and rejected button

--- scripts/utility.js
+++ scripts/utility.js
@@ -22,51 +22,100 @@ function showOne(id, btn) {
 
 // card section
 function interviewClick(id, card) {
+    // get the badge and card
     const badge = document.getElementById(id);
-    const addCard = document.getElementById(card);
+    const mainCard = document.getElementById(card);
+
+    // get the interview section
+    const interviewSection = document.getElementById('interview-section');
+
+    // interview count section
+    const interviewCount = document.getElementById('interview-count');
+    const rejectedCount = document.getElementById('rejected-count');
+
+    // check the card is already exist or not
+    if(mainCard.dataset.status === 'interview') {
+        return;
+    }
+    if(mainCard.dataset.status === 'rejected') {
+        const oldClone = document.getElementById(card + 'Cloned');
+        if(oldClone) {
+            oldClone.remove();
+        }
+        rejectedCount.innerText--;
+    }
+
+    // set the badge
     badge.innerText = 'INTERVIEW';
-    badge.classList.add('bg-green-600');
-    badge.classList.add('text-white');
+    badge.classList.add('bg-green-600', 'text-white');
     badge.classList.remove('bg-red-600');
 
-    const interviewCount = document.getElementById('interview-count');
-    const interviewCountValue = interviewCount.innerText;
-    let newCount = Number(interviewCountValue) + 1;
-    interviewCount.innerText = newCount;
+    // copy the main card and append it into interview section
+    const cloneCard = mainCard.cloneNode(true);
+    cloneCard.id = card + 'Cloned';
 
-    const interviewSection = document.getElementById('interview-section');
+    // append the card in interview section
+    interviewSection.appendChild(cloneCard);
+    mainCard.dataset.status = 'interview';
+
+    // count the interview section card
+    interviewCount.innerText = interviewSection.children.length - 1;
+
+    // hide the default interview display
     const defaultInterview = document.getElementById('default-interview');
-    if(newCount > 0) {
+
+    if(interviewSection.children.length > 0) {
         defaultInterview.classList.add('hidden');
     } else {
         defaultInterview.classList.remove('hidden');
     }
-    
-    const clonedCard = addCard.cloneNode(true);
-    interviewSection.appendChild(clonedCard);
-    
 }
 function rejectedClick(id, card) {
+    // get the badge and card
     const badge = document.getElementById(id);
-    const addCard = document.getElementById(card);
+    const mainCard = document.getElementById(card);
+
+    // get the interview section
+    const rejectionSection = document.getElementById('rejection-section');
+
+    // interview count section
+    const interviewCount = document.getElementById('interview-count');
+    const rejectedCount = document.getElementById('rejected-count');
+
+    // check the card is already exist or not
+    if(mainCard.dataset.status === 'rejected') {
+        return;
+    }
+    if(mainCard.dataset.status === 'interview') {
+        const oldClone = document.getElementById(card + 'Cloned');
+        if(oldClone) {
+            oldClone.remove();
+        }
+        interviewCount.innerText--;
+    }
+
+    // set the badge
     badge.innerText = 'REJECTED';
-    badge.classList.add('bg-red-600');
-    badge.classList.add('text-white');
+    badge.classList.add('bg-red-600', 'text-white');
     badge.classList.remove('bg-green-600');
 
-    const rejectedCount = document.getElementById('rejected-count');
-    const rejectedCountValue = rejectedCount.innerText;
-    let newCount = Number(rejectedCountValue) + 1;
-    rejectedCount.innerText = newCount;
+    // copy the main card and append it into interview section
+    const cloneCard = mainCard.cloneNode(true);
+    cloneCard.id = card + 'Cloned';
 
-    const rejectionSection = document.getElementById('rejection-section');
+    // append the card in rejection section
+    rejectionSection.appendChild(cloneCard);
+    mainCard.dataset.status = 'rejected';
+
+    // count the rejected section card
+    rejectedCount.innerText = rejectionSection.children.length - 1;
+
+    // hide the default rejection display
     const defaultRejection = document.getElementById('default-rejection');
-    if(newCount > 0) {
+
+    if(rejectionSection.children.length > 0) {
         defaultRejection.classList.add('hidden');
     } else {
         defaultRejection.classList.remove('hidden');
     }
-    
-    const clonedCard = addCard.cloneNode(true);
-    rejectionSection.appendChild(clonedCard);
 }
